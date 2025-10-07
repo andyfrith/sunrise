e5e774a8b5cff1d9846061607523ff8f04e9680a
Andy Frith <andy_frith@yahoo.com>
2025-10-07 17:04:37 -0600

    Install Playwright.  Add test spec for app.  Update Playwright config.  Add Playwright GitHub Action.

---
+++ e2e/app.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("has page header and counter button", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(
+    page.getByRole("heading", { name: "Vite + React" })
+  ).toBeVisible();
+
+  await expect(page.getByRole("button", { name: /^count is/ })).toBeVisible();
+});
