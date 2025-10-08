93984f46862d21b210abf8d88f0581b8de08647a
Andy Frith <andy_frith@yahoo.com>
2025-10-07 18:04:21 -0600

    Testing deployment prevention.

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -3,9 +3,7 @@ import { test, expect } from "@playwright/test";
 test("has page header and counter button", async ({ page }) => {
   await page.goto("/");
 
-  await expect(
-    page.getByRole("heading", { name: "Vite + React + Testerama" })
-  ).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Testerama!" })).toBeVisible();
 
   await expect(page.getByRole("button", { name: /^count is/ })).toBeVisible();
 });
